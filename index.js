ac086c5452489cbfedf60aa829b5d44809a324d7
ohjiyang <ohjiyung@icloud.com>
2023-05-19 02:24:56 +0600

    mongoose connection

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from "express"
 import cors from "cors"
+import mongoose from "mongoose";
+
+mongoose
+    .connect('mongodb+srv://nothinnnew:Fkon0071223@cluster0.tr3ppkm.mongodb.net/somenote?retryWrites=true&w=majority')
+    .then(() => console.log("DB OK"))
+    .catch((err) => console.log("DB error", err))
 
 const app = express()
 app.use(express.json())
